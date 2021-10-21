ae1d961e76e6160d08172325521a529afab1693d
Preyformeplz33 <patterson.eric.ep@gmail.com>
2021-10-21 02:12:13 -0500

    first commit

---
+++ script2.js
@@ -0,0 +1,37 @@
+var  num = 9;
+var numElement = document.querySelector("#num")
+console.log(numElement)
+
+
+function up(){
+    num++;
+    numElement.innerText = num
+    console.log(num);
+   
+
+}
+
+var numa = 12;
+var numaElement = document.querySelector("#numa")
+
+
+
+function upA(){
+    numa++;
+    numaElement.innerText = numa
+    console.log(numa);
+
+}
+
+
+var Opa = 9;
+var OpaElement = document.querySelector("#Opa")
+
+
+function opa(){
+    Opa++;
+    OpaElement.innerText = Opa
+    console.log(Opa);
+}
+
+
